d40ded348dbee7a7c9b65bd9c7f97ecde9421b83
phatdroid@gmail.com <phatdroid94@gmail.com>
2019-06-26 19:30:40 +0700

    a

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require("express")
 const app = express()
 
-app.get("/cong" , (req , res) => {
-    console.log("Day phuong thuc get")
+// truong hoac round
+app.get("/chia/:a/:b" , (req , res) => {
+    const {a , b} = req.params
+    if(isNaN(a) || isNaN(b)){
+       return res.send({success : false , message : "Not a number"})
+    }else if(b == 0){
+        return res.send({success : false , message : "DIVIDE ZERO"})
+    }
+    const ketqua = +a + +b
+    res.send({success : true , ketqua})
 })
 
 
