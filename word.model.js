384df40fd5ae10d874bc361cf10a5e35d24a6589
phatdroid@gmail.com <phatdroid94@gmail.com>
2019-06-28 18:29:07 +0700

    a

---
+++ word.model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://abc_1:ABC123456@databasenode-fx0di.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true});
+
+const Word = mongoose.model('Word', { 
+    en : {type : String , require : true , trim : true , unique : true},
+    vn : {type : String , require : true , trim : true },
+    isMemorized : {type : Boolean , require : true , default : false},
+});
+
+Word.find({})
+.then(words => console.log(words))
